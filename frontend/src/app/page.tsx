4e27dac9b14a3412836cd7c9224e02a7b2208fed
hukaiems <nguoinhen0105@gmail.com>
2025-08-03 14:50:49 +0700

    finish the register page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,32 +1,57 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import { useRouter } from 'next/navigation';
 import Webcam from "react-webcam";
 import axios from "axios";
 
-const sendScreenshot = async (base64Image: string[]) => {
-  try{
-    const response = await axios.post("http://localhost:8000/compare", {
-      images: base64Image,
-    })
-    console.log("Response:", response.data);
-  } catch (err: any) {
-  console.log("Error details:", err.response?.data);
+export interface RecognizedResponse {
+  recognized: boolean;
+  user_id: string | null;
+  similarity: number;
+  votes: number;
 }
-}
+
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL!;
+if (!API_BASE) throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
 
 export default function Home() {
   const [showCam, setShowCam] = useState(false);
   const webcamRef = useRef<Webcam>(null); // to interact with the webcam instance
   const [screenshot, setScreenshot] = useState<string[]>([]);
+  const [result, setResult] = useState<RecognizedResponse | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+  const router = useRouter();
+
+  const sendScreenshot = async (
+    base64Image: string[]
+  ): Promise<RecognizedResponse | null> => {
+    try {
+      const response = await axios.post<RecognizedResponse>(
+        `${API_BASE}/compare`,
+        {
+          images: base64Image,
+        },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      console.log("Response:", response.data);
+
+      return response.data;
+    } catch (err: any) {
+      console.log("Error details:", err.response?.data);
+      const detail = err.response?.data?.detail ?? "Unknown error";
+      setErrorMsg(detail);
+      return null;
+    }
+  };
 
   useEffect(() => {
     const captureImages = async () => {
       if (showCam) {
         if (webcamRef.current) {
           const captured: string[] = [];
-          await delay(2000);
+          await delay(5000);
           for (let i = 0; i < 5; i++) {
             const img = webcamRef.current.getScreenshot();
             if (img) {
@@ -36,45 +61,60 @@ export default function Home() {
             await delay(500);
           }
           setScreenshot(captured);
-          await sendScreenshot(captured);
+          const res = await sendScreenshot(captured);
           setShowCam(false);
+          setResult(res);
         }
       }
     };
     captureImages();
   }, [showCam]);
 
-  useEffect(() => {
-    if (screenshot) {
-      setShowCam(false);
-    }
-  }, [screenshot]);
+  const goToRegister = () => {
+    router.push('/register');
+  };
 
   return (
     // needs height for centering
     <div className="flex flex-col justify-center items-center h-screen gap-4">
-      <h1 className="text-4xl font-bold bg-gradient-to-r from-sky-400 to-indigo-500 text-transparent bg-clip-text p-10 rounded-xl">
+      <h1 className="text-4xl font-bold bg-gradient-to-r from-sky-400 to-indigo-500 text-transparent bg-clip-text p-10 pb-0 rounded-xl">
         Facial authentication website
       </h1>
       {!showCam && screenshot.length === 0 && (
-        <button
-          className="border rounded-2xl p-3 hover:bg-sky-700"
-          onClick={() => setShowCam(true)}
-        >
-          Login
-        </button>
+        <div className="flex gap-3">
+          <button
+            className="border rounded-2xl p-3 hover:bg-sky-700"
+            onClick={() => setShowCam(true)}
+          >
+            Login
+          </button>
+          <button
+            className="border rounded-2xl p-3 hover:bg-sky-700"
+            onClick={goToRegister}
+          >
+            Register
+          </button>
+        </div>
       )}
 
       {/* show cam sections */}
       {showCam && (
         <div className="flex flex-col justify-center items-center gap-3">
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            videoConstraints={{ facingMode: "user" }}
-            className="rounded-xl shadow-lg"
-          />
+          <h1 className="text-4xl">Put your face in the circle</h1>
+          <div className="relative rounded-xl">
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={{ facingMode: "user" }}
+              className="shadow-lg"
+            />
+            {/* 📌 Overlay: absolute, no pointer events */}
+            <div className="absolute inset-0 flex justify-center items-center pointer-events-none">
+              {/* adjust w-3/4 h-3/4 to control size of the oval */}
+              <div className="border-4 border-white rounded-full w-2/4 h-4/4" />
+            </div>
+          </div>
 
           <button
             className="bg-white w-24 text-black rounded-xl h-12 hover:bg-yellow-500"
@@ -83,22 +123,36 @@ export default function Home() {
               setScreenshot([]);
             }}
           >
-            Cancle{" "}
+            Cancel
           </button>
         </div>
       )}
 
-      {/* picture screenshot */}
+      {/* Error face too far */}
+      {errorMsg && <h1>{errorMsg}</h1>}
+
+      {/* picture screenshot  and authentication result*/}
       {screenshot.length > 0 && (
         <div className="flex gap-3">
-          {screenshot.map((src, idx) => (
-            <img 
-              key={idx}
-              src={src}
-              alt={`Screenshot ${idx + 1}`}
-              className="rounded-x max-w-xs"
-            />
-          ))}
+          <img
+            src={screenshot[3]}
+            alt="Screenshot 1"
+            className="rounded-xl max-w-xs"
+          />
+          {result?.recognized === true && (
+            <div>
+              <h1>Name: {result.user_id}</h1>
+              <h1>Similarity score: {result?.similarity}</h1>
+              <h1>Votes: {result.votes}</h1>
+            </div>
+          )}
+
+          {result?.recognized === false && (
+            <div>
+              <h1>Can't recognized user.</h1>
+              <h1>Similarity score: {result?.similarity}</h1>
+            </div>
+          )}
         </div>
       )}
     </div>
---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,178 @@
+"use client";
+import Webcam from "react-webcam";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL!; //not null or undefined
+
+interface RegisterResponse {
+  status: string;
+  user_id: string;
+  registered_poses: string[];
+}
+
+export default function Register() {
+  const [userName, setUserName] = useState<string>("");
+  const [showCam, setShowCam] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [instruction, setInstruction] = useState<string>("");
+  const [screenShot, setScreenShot] = useState<string[]>([]);
+  const [serverResponse, setServerResponse] = useState<RegisterResponse | null>(
+    null
+  );
+  const [error, setError] = useState<string>("");
+  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+  const router = useRouter();
+
+  // handle userName change
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setUserName(e.target.value);
+  }
+
+  function handleBlur() {
+    if (userName.trim()) {
+      //check to see if user type something after trim
+      setShowCam(true);
+    }
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter" && userName.trim()) {
+      (e.target as HTMLInputElement).blur();
+      setShowCam(true);
+    }
+  }
+
+  const sendData = async (
+    images: string[],
+    userName: string
+  ): Promise<RegisterResponse | null> => {
+    try {
+      const formData = new FormData();
+      formData.append("user_id", userName);
+
+      const poseNames = ["frontal", "left", "right"];
+      for (let i = 0; i < 3; i++) {
+        const base64 = images[i];
+        const blob = await (await fetch(base64)).blob();
+        formData.append(poseNames[i], blob, `${poseNames[i]}.jpg`);
+      }
+
+      const response = await axios.post<RegisterResponse>(
+        `${API_BASE}/register`,
+        formData,
+        { headers: { "Content-Type": "multipart/form-data" } }
+      );
+      console.log("Backend response:", response.data);
+      return response.data;
+    } catch (err: any) {
+      setError("Failed try again!");
+      setShowCam(false);
+      return null;
+    }
+  };
+
+  //   interact with camera after showCam change
+  useEffect(() => {
+    const registerImages = async () => {
+      if (showCam && webcamRef.current) {
+        const captured: string[] = [];
+        setInstruction("Put your face in the circle ");
+        await delay(4000);
+        // first shot
+        setInstruction("Look in front of the camera");
+        await delay(4000);
+        let img = webcamRef.current.getScreenshot();
+        if (img) {
+          captured.push(img);
+          console.log(`Captured photo!`);
+        }
+        // second shot
+        setInstruction(" Lean your face a little bit to the left");
+        await delay(4000);
+        img = webcamRef.current.getScreenshot();
+        if (img) {
+          captured.push(img);
+          console.log(`Captured photo!`);
+        }
+        // third shot
+        setInstruction(" Lean your face a little bit to the right");
+        await delay(4000);
+
+        img = webcamRef.current.getScreenshot();
+        if (img) {
+          captured.push(img);
+          console.log(`Captured photo!`);
+        }
+        setScreenShot(captured);
+        const result = await sendData(captured, userName);
+        if (result) {
+          setServerResponse(result);
+        }
+      }
+    };
+
+    registerImages();
+  }, [showCam]);
+
+  const goToHome = () => {
+    router.push("/");
+  };
+
+  return (
+    // main div
+    <div>
+      {/* input section */}
+      {!showCam ? (
+        <div className="flex flex-col justify-center items-center h-screen gap-4">
+          {screenShot.length > 0 && !serverResponse && (
+            <h1>
+              Registration failed, move your face closer to the camera next time, try again.
+            </h1>
+          )}
+          <h1 className="text-4xl">Enter your name below</h1>
+          <input
+            placeholder="Enter your name"
+            value={userName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onKeyDown={handleKeyDown}
+            className="border rounded-lg"
+            disabled={showCam} //stop people to change input after show the Cam
+          ></input>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center gap-3">
+          <h1 className="text-4xl">{instruction}</h1>
+          <div className="relative rounded-xl">
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={{ facingMode: "user" }}
+              className="shadow-lg rounded-xl"
+            />
+            {/* 📌 Overlay: absolute, no pointer events */}
+            <div className="absolute inset-0 flex justify-center items-center pointer-events-none">
+              {/* adjust w-3/4 h-3/4 to control size of the oval */}
+              <div className="border-4 border-white rounded-full w-2/4 h-4/4" />
+            </div>
+          </div>
+        </div>
+      )}
+
+      {serverResponse && (
+        <div className="flex flex-col gap-3 pt-4 justify-center items-center">
+          <h1>User registered as: {serverResponse.user_id}</h1>
+          <button
+            className="border p-2 rounded-xl hover:bg-gray-100/40"
+            onClick={goToHome}
+          >
+            Back to home page
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
